49952e1813b44a66026a81767be34c932b216dcc
hunterrudnet <hunter.rudnet@gmail.com>
2023-02-15 09:34:04 -0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
+import Profile from './components/Profile';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <LoginButton />
+        <LogoutButton />
+        <Profile />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SearchExpandedCard.js
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import '@fontsource/roboto/300.css';
+
+export default function SearchExpandedCard({ album }) {
+  const theme = useTheme();
+
+  return (
+    <Card sx={{ display: 'flex', m: 2, borderRadius: '16px'}}>
+      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+        <CardContent>
+          <Typography component="div" variant="h5">
+            {album.name} - {album.artist} 
+            {album.attributes.map((attribute) => (
+              <Typography display='inline' bgcolor='lightgrey' sx={{ p:.3, m: 1, borderRadius: '16px', border: '1px solid grey', color: theme.palette.text.secondary }}>{attribute}</Typography>
+            ))}
+            <IconButton aria-label="add to favorites">
+            <FavoriteIcon sx={{ pl: 8, height: 38, width: 38 }}/>
+          </IconButton>
+          </Typography>
+        </CardContent>
+        <Box sx={{ display: 'flex', pl: 1, pb: 1 }}>
+          <IconButton aria-label="play/pause" href={album.url}>
+            <PlayArrowIcon sx={{ height: 38, width: 38 }} />
+            <Typography variant="subtitle1" color="text.secondary" component="div">
+            Play on Spotify
+          </Typography>
+          </IconButton>
+        </Box>
+      </Box>
+      <CardMedia
+        component="img"
+        sx={{ width: 151 }}
+        image={album.image}
+        alt="Live from space album cover"
+      />
+    </Card>
+  );
+}
