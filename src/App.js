73709153b35d64de79dd47a842a52bb999b69f65
hunterrudnet <hunter.rudnet@gmail.com>
2023-02-25 19:22:14 -0500

    add see-more route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import Profile from './components/Profile/Profile';
 import {BrowserRouter} from "react-router-dom";
 import {Routes, Route} from "react-router";
 import TopBar from "./components/TopBar/TopBar";
+import SeeMoreList from "./components/SeeMoreList/SeeMoreList.tsx";
+import {SEE_MORE_MOCK_DATA} from "./components/TestData/seeMoreMockData.tsx";
+
+const props = {
+  title: "Popular Albums",
+  items: SEE_MORE_MOCK_DATA
+}
 
 function App() {
   return (
@@ -10,6 +17,7 @@ function App() {
           <TopBar/>
           <Routes>
             <Route index element={null}/>
+            <Route path="/see-more" element={<SeeMoreList {...props}/>}/>
             <Route path="/profile" element={<Profile/>}/>
           </Routes>
         </div>
