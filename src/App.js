4e41337e013cae37b1bb0d168a6bafc27f01129d
hadeel-farhan <hadeelf2001@gmail.com>
2023-02-28 17:26:19 -0500

    merged main

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-import LoginButton from './components/LoginButton';
-import LogoutButton from './components/LogoutButton';
-import Profile from './components/Profile';
-
+// import LoginButton from './components/LoginButton';
+// import LogoutButton from './components/LogoutButton';
+import Profile from './components/Profile/Profile.js';
+import * as React from 'react';
+import Slides from "./components/Carousel/Cards.js"
+import SearchExtended from "./components/Carousel/SearchExtended.js"
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <LoginButton />
-        <LogoutButton />
+        {/* <LoginButton />
+        <LogoutButton /> */}
         <Profile />
+        {/* <SearchExtended />
+        <Slides /> */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
