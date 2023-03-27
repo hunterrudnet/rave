4bbc780a360e615eac4c0a3f935179fc06099535
John McCarthy <johnmccarthy522@gmail.com>
2023-03-27 13:22:49 -0400

    Front end framing for homepage (no backend connectivity)

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import SeeMoreList from "./components/SeeMoreList/SeeMoreList.tsx";
 import { SEE_MORE_MOCK_DATA } from "./components/TestData/seeMoreMockData.tsx";
 import Details from './components/Details/Details.js'
 import SearchPage from './components/Search/SearchPage';
+import HomePage from "./components/Home/HomePage";
 const props = {
   title: "Popular Albums",
   items: SEE_MORE_MOCK_DATA
@@ -17,7 +18,7 @@ function App() {
       <div>
         <TopBar />
         <Routes>
-          <Route index element={null} />
+          <Route index element={<HomePage />} />
           <Route path="/see-more" element={<SeeMoreList {...props} />} />
           <Route path="/profile" element={<Profile />} />
           <Route path='/search' element={<SearchPage />} />
