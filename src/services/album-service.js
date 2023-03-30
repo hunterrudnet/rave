9aa640d9d2f74b242b527a77497c56d7ad7589f2
hunterrudnet <hunter.rudnet@gmail.com>
2023-03-30 08:53:59 -0400

    Add the rest of the album-service routes for others to use in their pages

--- src/services/album-service.js
+++ src/services/album-service.js
@@ -2,6 +2,21 @@ import axios from 'axios';
 
 const ALBUM_API = process.env.REACT_APP_BASE_API + '/albums';
 
+export const getAlbumBySpotifyId = async (spotifyId) => {
+  const response = await axios.get(`${ALBUM_API}/${spotifyId}`);
+  return response.data;
+};
+
+export const getAverageReviewScoreByAlbumId = async (albumId) => {
+  const response = await axios.get(`${ALBUM_API}/review/${albumId}`);
+  return response.data;
+};
+
+export const getAlbumByAlbumId = async (albumId) => {
+  const response = await axios.get(`${ALBUM_API}/lookup/${albumId}`);
+  return response.data;
+};
+
 export const getAlbumSearch = async (query) => {
   const response = await axios.get(`${ALBUM_API}/search/${query}`);
   return response.data;
