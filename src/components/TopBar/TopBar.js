dd0e3a8b29af2123bdb7bdb77d18bfb009b5c983
hadeel-farhan <hadeelf2001@gmail.com>
2023-04-02 14:10:25 -0400

    fixed linking issue

--- src/components/TopBar/TopBar.js
+++ src/components/TopBar/TopBar.js
@@ -16,16 +16,16 @@ const TopBar = () => {
         <AppBar position="static">
           <Toolbar>
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              <Link className="top-bar-link" to="/">
-                <Box sx={{ position: 'relative', width: 70, height: '100%', display: 'flex', alignItems: 'center' }}>
+              <Box sx={{ position: 'relative', width: 70, height: '100%', display: 'flex', alignItems: 'center' }}>
+                <Link className="top-bar-link" to="/">
                   <CardMedia
                     component="img"
                     sx={{ position: 'relative', top: 0, p: 0, right: 0, bottom: 0, left: 0, objectFit: 'cover', borderRadius: 1 }}
                     image="/images/rave-logo.jpg"
                     alt="logo"
                   />
-                </Box>
-              </Link>
+                </Link>
+              </Box>
             </Typography>
             <LoginToggleButton />
           </Toolbar>
