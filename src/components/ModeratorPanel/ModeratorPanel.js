083387e6113eb5cf8519d6082ad1177afcc4613e
hunterrudnet <hunter.rudnet@gmail.com>
2023-04-02 11:20:19 -0400

    Access correct fields when reading response, and redirect to the home page if the user is not a mod

--- src/components/ModeratorPanel/ModeratorPanel.js
+++ src/components/ModeratorPanel/ModeratorPanel.js
@@ -5,40 +5,39 @@ import "../Reused/reused.css";
 import { useSelector } from "react-redux";
 import { getAllReviews, deleteReview } from "../../services/reviews-service";
 import { Navigate } from "react-router-dom";
-import Reviews from "../Reviews/Reviews";
 import Typography from "@mui/material/Typography";
 
 const ModeratorPanel = () => {
 
-  let { loggedInUser, loading, loggedIn } = useSelector(
-    state => state.loggedInUserData);
+  let { loggedInUser, loading, loggedIn } = useSelector(state => state.loggedInUserData);
   const [reviewsLoading, setReviewsLoading] = useState(true);
-  const [reviewsData, updateReviewsData] = useState([]);
+  const [reviewsData, setReviewsData] = useState([]);
 
   const fetchReviewsData = async () => {
     //get all reviews
     const reviews = await getAllReviews();
-    updateReviewsData(reviews);
+    setReviewsData(reviews);
     setReviewsLoading(false);
   };
 
   useEffect(() => {
     if (!loading) {
       setReviewsLoading(true);
-      updateReviewsData([]);
+      setReviewsData([]);
       fetchReviewsData();
     }
   }, [loading, loggedInUser, loggedIn]);
 
   const handleDelete = async (id) => {
-    const reviews = await deleteReview(id);
-    updateReviewsData(reviews);
-    setReviewsLoading(false);
+    await deleteReview(id);
+    let newReviews = reviewsData.filter((review) => review.id !== id);
+    setReviewsData(newReviews);
   };
 
   if (loading) {
     return <div>Loading ...</div>;
-  } else if (!loading && !loggedIn) {
+    // If they try to hit /moderator and aren't a mod, just redirect them to the home page
+  } else if ((!loading && !loggedIn) || (loggedInUser && !loggedInUser.isMod)) {
     return <Navigate replace to={"/"} />;
   } else {
     return (
@@ -47,7 +46,7 @@ const ModeratorPanel = () => {
         <List>
           {reviewsData.map((review) => (
             <ListItem key={review.id}>
-              <ListItemText primary={review.text} />
+              <ListItemText primary={review.reviewText} />
               <ListItemSecondaryAction>
                 <IconButton edge="end" aria-label="delete" onClick={() => handleDelete(review.id)}>
                   <DeleteIcon />
@@ -57,9 +56,6 @@ const ModeratorPanel = () => {
           ))}
         </List>
       </>
-
-      // <Reviews reviews={reviewsData} loading={reviewsLoading} />
-
     );
   }
 };
