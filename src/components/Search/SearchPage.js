fe0a49bb00bc6db5b97c9d622778c78d4b9999c3
hunterrudnet <hunter.rudnet@gmail.com>
2023-04-03 21:36:49 -0400

    Style search page

--- src/components/Search/SearchPage.js
+++ src/components/Search/SearchPage.js
@@ -5,38 +5,58 @@ import {
   TextField,
   IconButton,
   List,
-  InputAdornment
+  InputAdornment,
+  CircularProgress,
+  Typography,
+  createTheme,
+  ThemeProvider,
+  Divider,
 } from '@mui/material';
 import { Search } from '@mui/icons-material';
 import SearchExpandedCard from './SearchExpandedCard';
 import { getAlbumSearch } from '../../services/album-service';
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#2196f3',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+  },
+});
+
 const SearchPage = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   const handleChange = (event) => {
     setSearchTerm(event.target.value);
   };
 
   const handleSearch = async () => {
+    setLoading(true);
     try {
-
       const results = await getAlbumSearch(searchTerm);
 
-      let albums = [];
-      for (let i = 0; i < Math.min(results.length, 10); i++) {
-        let album = results[i];
-        let spotifyId = album.spotifyId;
-        let name = album.name;
-        let artist = album.artists[0].name;
-        let image = album.images[1].url;
-        let url = album.external_urls.spotify;
-        albums.push({ spotifyId, name, artist, image, url });
-      }
+      const albums = results.slice(0, 10).map((album) => ({
+        spotifyId: album.spotifyId,
+        name: album.name,
+        artist: album.artists[0].name,
+        image: album.images[1].url,
+        url: album.external_urls.spotify,
+      }));
+
       setResults(albums);
+      setLoading(false);
+      setError(null);
     } catch (error) {
       console.error('Error fetching search results:', error);
+      setLoading(false);
+      setError(error.message);
     }
   };
 
@@ -47,33 +67,56 @@ const SearchPage = () => {
   };
 
   return (
-    <Container>
-      <Container sx={{m: 1}}>
-      <TextField
-        label="Search"
-        placeholder="Search for an album, artist, or keyword"
-        variant="outlined"
-        value={searchTerm}
-        onChange={handleChange}
-        onKeyPress={handleKeyPress}
-        fullWidth
-        InputProps={{
-          endAdornment: (
-            <InputAdornment position="end">
-              <IconButton onClick={handleSearch}>
-                <Search />
-              </IconButton>
-            </InputAdornment>
-          ),
-        }}
-      />
+    <ThemeProvider theme={theme}>
+      <Container sx={{ padding: '24px' }}>
+        <Typography variant="h4" sx={{ marginBottom: '16px' }}>
+          Search for an album, artist, or keyword
+        </Typography>
+        <TextField
+          label="Search"
+          placeholder="Search"
+          variant="outlined"
+          value={searchTerm}
+          onChange={handleChange}
+          onKeyPress={handleKeyPress}
+          fullWidth
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton onClick={handleSearch} disabled={loading}>
+                  <Search />
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+          sx={{ marginBottom: '16px' }}
+        />
+        <Divider sx={{ marginBottom: '16px' }} />
+        {loading && (
+          <Typography variant="body1" sx={{ textAlign: 'center', marginBottom: '16px' }}>
+            <CircularProgress size={32} color="secondary" sx={{ marginRight: '8px' }} />
+            Searching...
+          </Typography>
+        )}
+        {error && (
+          <Typography variant="body1" sx={{ color: 'error.main', marginBottom: '16px' }}>
+            {error}
+          </Typography>
+        )}
+        {results.length > 0 && (
+          <List sx={{ marginBottom: '16px' }}>
+            {results.map((result, index) => (
+              <SearchExpandedCard key={result.spotifyId} index={index} album={result} />
+            ))}
+          </List>
+        )}
+        {results.length === 0 && !loading && (
+          <Typography variant="body1" sx={{ textAlign: 'center', marginBottom: '16px' }}>
+            No results found.
+          </Typography>
+        )}
       </Container>
-      <List>
-        {results.map((result, index) => (
-          <SearchExpandedCard index={index} album={result} />
-        ))}
-      </List>
-    </Container>
+    </ThemeProvider>
   );
 };
 
